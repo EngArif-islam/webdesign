28add92faf42fec73a63ddc0aa3c502e2fb33a74
EngArif-islam <90537017+EngArif-islam@users.noreply.github.com>
2021-09-12 08:03:55 +0600

    added html and css file for practice

---
+++ Tilted Page Scroll/js/active.js
@@ -0,0 +1,14 @@
+(function($){
+	"use strict";
+	/*------------------------
+	title page scroll active
+	--------------------------*/
+	$('.main') .tiltedpage_scroll({
+		sectionContainer: ">section",
+		angle:20,
+		opacity:true,
+		scale:true,
+		outAnimation:true,
+
+	});
+})(jQuery);
